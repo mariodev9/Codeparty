c50e84c06c93e48187276ccef6c9edb33a4f6438
LucianoMariotti <luciano.mariotti99@gmail.com>
2022-08-04 02:17:00 -0300

    fix svg

--- components/Icons/Photo.js
+++ components/Icons/Photo.js
@@ -8,8 +8,8 @@ export default function Photo({ color }) {
       width="30"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g fill="none" fill-rule="evenodd" transform="translate(3 3)">
-        <g stroke={color} stroke-linecap="round" stroke-linejoin="round">
+      <g fill="none" fillRule="evenodd" transform="translate(3 3)">
+        <g stroke={color} strokeLinecap="round" strokeLinejoin="round">
           <path d="m2.5.5h10c1.1045695 0 2 .8954305 2 2v10c0 1.1045695-.8954305 2-2 2h-10c-1.1045695 0-2-.8954305-2-2v-10c0-1.1045695.8954305-2 2-2z" />
           <path d="m14.5 10.5-3-3-3 2.985" />
           <path d="m12.5 14.5-9-9-3 3" />
